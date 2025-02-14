9cdf7e8a38c6abb2ce4458ca5bbb14f7b3b3344c
Michael Stebel <mdstebel@gmail.com>
2025-02-14 13:13:36 -0500

    Update BezierCurveShape.swift

--- BezierCurveShape.swift
+++ BezierCurveShape.swift
@@ -1,3 +1,9 @@
+// This SwiftUI example defines and visualizes a generalized Bézier curve function for three or more control points.
+// The code includes helper functions to compute the binomial coefficient and a single point on the curve using the Bernstein basis.
+// A custom Shape, BezierCurveShape, is then used within a SwiftUI view to draw both the control polygon and the Bézier curve.
+// Michael Stebel
+// 2/13/2025
+
 import SwiftUI
 
 // Compute the binomial coefficient ("n choose k")
